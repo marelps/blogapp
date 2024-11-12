3198aa25f6da3495047b10154f202368330bf7f2
Vitória Garrucho <vitoriagarrucho@gmail.com>
2024-11-12 12:44:00 -0300

    listando categorias

--- routes/admin.js
+++ routes/admin.js
@@ -14,7 +14,12 @@ router.get('/posts', (req,res) =>{
 })
 
 router.get('/categorias', (req,res) => {
-    res.render("admin/categorias")
+    Categoria.find().sort({date:'desc'}).then((categorias) => {
+        res.render("admin/categorias", {categorias: categorias})
+    }).catch((err) => {
+        req.Flash("error_msg", "Houve um erro ao listar categorias")
+        res.redirect("/admin")
+    })
 })
 
 router.get('/categorias/add', (req,res) =>{
@@ -22,19 +27,36 @@ router.get('/categorias/add', (req,res) =>{
 })
 
 router.post('/categorias/nova', (req, res) => {
-    const novaCategoria = new Categoria({
+
+    var erros = []
+
+    if(!req.body.nome || typeof req.body.nome == undefined || req.body.nome == null){
+        erros.push({texto: "Nome Inválido"})
+    }
+
+    if(!req.body.slug || typeof req.body.slug == undefined || req.body.slug == null){
+        erros.push({texto: "Slug Inválido"})
+    }
+
+    if(req.body.nome.length < 2){
+        erros.push({texto: "Nome da catagoria é muito pequena"})
+    }
+
+    if(erros.length > 0){
+        res.render("admin/addcategorias", {erros: erros})
+    }
+
+    const novaCategoria = {
         nome: req.body.nome,
         slug: req.body.slug
-    });
+    };
 
-    novaCategoria.save()
-        .then(() => {
+    new Categoria(novaCategoria).save().then(() => {
             console.log('Categoria salva com sucesso!')
-            res.redirect('/admin/categorias'); // Adicione um redirect após salvar
-        })
-        .catch((err) => {
+            // res.redirect('/admin/categorias'); // Adicione um redirect após salvar
+        }).catch((err) => {
             console.log(`Error: ${err}`)
-            res.redirect('/admin/categorias'); // Redirecione mesmo após erro
+           // res.redirect('/admin/categorias'); // Redirecione mesmo após erro
         });
 });
 
